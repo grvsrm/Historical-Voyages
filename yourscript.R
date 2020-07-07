b7961b3812e0e7da5ce494f4393c4fdd57de0022
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-07-07 21:52:46 +0530

    made changes in dot chart

--- yourscript.R
+++ yourscript.R
@@ -9,3 +9,6 @@ summary(VADeaths)
 #' this chunk, so the resulting PNG has a decent name.
 #+ dotchart
 dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
+
+#+ Another dotchart
+dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
